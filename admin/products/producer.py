d83681961d6c063955a1444c18d344007d9834a3
SeaSkyThe <marceloer2011@gmail.com>
2022-07-20 15:09:15 -0300

    Finishing comunnication between the services

--- admin/products/producer.py
+++ admin/products/producer.py
@@ -1,4 +1,4 @@
-import pika
+import pika, json
 
 params = pika.URLParameters("amqps://zrtzvwlx:aJ47ugn5XMiilSgbxNFjCjisbwJhoLuz@gull.rmq.cloudamqp.com/zrtzvwlx")
 
@@ -6,5 +6,6 @@ connection = pika.BlockingConnection(params)
 
 channel = connection.channel()
 
-def publish():
-    channel.basic_publish(exchange='', routing_key='main', body='hello')
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
--- main/consumer.py
+++ main/consumer.py
@@ -1,3 +1,5 @@
+import json
+from main import Product, db
 import pika
 
 params = pika.URLParameters("amqps://zrtzvwlx:aJ47ugn5XMiilSgbxNFjCjisbwJhoLuz@gull.rmq.cloudamqp.com/zrtzvwlx")
@@ -9,12 +11,32 @@ channel = connection.channel()
 channel.queue_declare(queue='main')
 
 
-def callback(channel, method, properties, body):
-    print('Received in main')
-    print(body)
+def callback(ch, method, properties, body):
+    print('\nReceived in main')
+    data = json.loads(body)
+    print(data)
 
+    if(properties.content_type == 'product_created'):
+        product = Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+        print('Product Created')
 
-channel.basic_consume(queue='main', on_message_callback=callback)
+    elif(properties.content_type == 'product_updated'):
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+        print('Product Updated')
+
+    elif(properties.content_type == 'product_deleted'):
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
+        print('Product Deleted')
+    
+    
+channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
 
 print('Started Consuming')
 
